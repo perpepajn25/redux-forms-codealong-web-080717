13646a75f526af053ef35a754ac1338c28f7720e
Natalie Perpepaj <perpepajn25@gmail.com>
2017-10-27 15:19:29 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import createStore from './createStore';
 
 let store = createStore(manageTodo);
 
-export function render(){
-  ReactDOM.render(
-    <App store={store} />,
-    document.getElementById('root')
-  );
-};
+export function render() {
+ ReactDOM.render(
+   <App store={store} />,
+   document.getElementById('root')
+ );
+}
 
-store.dispatch({type: '@@INIT'});
+store.dispatch({ type: '@@INIT' });
